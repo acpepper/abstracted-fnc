979737829d65c5d0e37a15b63842e2b1f240eb87
Andrew Pepper <andrewpepper96@gmail.com>
2022-02-01 13:37:48 -0800

    docs: comments

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ export function newFunc(a: number, b: number, coeffs: Array<number>) {
   return (coeffs[0] * a + coeffs[1]) * (coeffs[2] * b + coeffs[3]);
 }
 
+// some comment
 export function newApp1(a: number, b: number) {
   return newFunc(Number(a), Number(b), [-1, 2, 1, 1]);
 }
 
+// another one
 export function newApp2(a: number, b: number) {
   return newFunc(a, b, [1, -1, 1, 2]);
 }
