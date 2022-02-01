815a49694d515c11712a59df656bfaa69cc6af80
Andrew Pepper <andrewpepper96@gmail.com>
2022-02-01 13:36:05 -0800

    feat: abstraction

---
+++ index.ts
@@ -0,0 +1,15 @@
+export function newFunc(a: number, b: number, coeffs: Array<number>) {
+  return (coeffs[0] * a + coeffs[1]) * (coeffs[2] * b + coeffs[3]);
+}
+
+export function newApp1(a: number, b: number) {
+  return newFunc(a, b, [-1, 2, 1, 1]);
+}
+
+export function newApp2(a: number, b: number) {
+  return newFunc(a, b, [1, -1, 1, 2]);
+}
+
+console.log(
+  newApp1(Number(process.argv.slice(2)[0]), Number(process.argv.slice(2)[1]))
+);
